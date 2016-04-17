a4e116410b3e1f7184fd698ed6feb583aa439895
lianghang <1271435657@qq.com>
2016-04-17 11:15:34 +0800

    梁行

--- material/test/666.py
+++ material/test/666.py
@@ -1,1 +1,14 @@
-print("hello,world")
+# print("hello,world")
+# i = 0
+# while i < 100:
+#     i = i + 1
+#     if i % 20 == 0:
+#         print(str(i) + "能被20整除")
+#         print(i)
+# a=3
+# if a == 2:
+#     print("this is true a = a")
+#     print("hahhah")
+# else:
+#     print("nononono")
+
