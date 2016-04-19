68cdd921ec9f5ba3f9efe21dbe885808c94be554
Yanyu <Yanyu>
2016-04-19 12:56:48 +0800

    2016.4.18.di6zhang

---
+++ Li-yun-quan/2016.4.18.di6zhang.py
@@ -0,0 +1,19 @@
+#1.使用format
+cm  = 170
+name = 'Tom'
+print('{0} have {1} cm'.format(name, cm))
+print('结束第一段程序!')
+#2.使用变量和字面常量
+x = 10
+print(x)
+x = x + 10
+print(x)
+y = '结束第二段程序！'
+print(y)
+#3.逻辑行和物理行
+a = 5
+print(a)
+b = 10
+print(b)
+c = 'This is very interesting!结束第三段程序。'
+print(c)
