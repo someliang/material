6ace8f87f63767d5d318584635e6b47ec568bea6
Rita <rita@qq.com>
2016-11-16 21:42:00 +0800

    将库存数量设置为不能操作。

--- flow/admin.py
+++ flow/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
 from .models import AddMaterial, InitMaterial
 
+class InitMaterialAdmin(admin.ModelAdmin):
+    fields = ['stocks','material', 'class_room']
+    readonly_fields = ['stocks']
+
 admin.site.register(AddMaterial)
-admin.site.register(InitMaterial)
+admin.site.register(InitMaterial, InitMaterialAdmin)
