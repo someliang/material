547f82535ed093e511c0d7fae33b0020ec76d598
Rita Liang <rita@qq.com>
2016-11-25 18:15:26 +0800

    添加自定义方法，可直接在列表处同意耗材申请。

--- flow/admin.py
+++ flow/admin.py
@@ -15,24 +15,22 @@ class AddMaterialAdmin(admin.ModelAdmin):
     def get_queryset(self, request):
         return super(AddMaterialAdmin, self).get_queryset(request).filter(material_info__class_room__admin__user=request.user)
 
-class ApplyBuyMaterialAdmin(admin.ModelAdmin):
+def agree_application(self, request, queryset):
+    queryset.update(is_agree=True)
 
-    fields = ['class_room', 'material', 'number', 'unit']
+    for obj in queryset:
+        material_info = InitMaterial.objects.filter(material=obj.material).get(class_room=obj.class_room)
+        material_info.stocks = material_info.stocks - obj.number
+        material_info.save()
 
-    def save_model(self, request, obj, form, change):
-
-        #TODO:这里需要再检验用户是否为管理员权限
-        if change and (obj.class_room.admin.user != request.user):
-            obj.is_agree = True
-            super(ApplyBuyMaterialAdmin, self).save_model(request, obj, form, change)
-        else:
-            obj.applicant = request.user
-            super(ApplyBuyMaterialAdmin, self).save_model(request, obj, form, change)
+agree_application.short_description = _("agree the materil applicant")
 
 class ApplyMaterialAdmin(admin.ModelAdmin):
 
     fields = ['class_room', 'material', 'number']
     list_display = ['class_room', 'material', 'number', 'is_agree', 'apply_time', 'applicant']
+    actions = [agree_application]
+
 
     def save_model(self, request, obj, form, change):
 
@@ -59,6 +57,21 @@ class ApplyMaterialAdmin(admin.ModelAdmin):
 
 
 
+
+
+class ApplyBuyMaterialAdmin(admin.ModelAdmin):
+
+    fields = ['class_room', 'material', 'number', 'unit']
+
+    def save_model(self, request, obj, form, change):
+
+        #TODO:这里需要再检验用户是否为管理员权限
+        if change and (obj.class_room.admin.user != request.user):
+            obj.is_agree = True
+            super(ApplyBuyMaterialAdmin, self).save_model(request, obj, form, change)
+        else:
+            obj.applicant = request.user
+            super(ApplyBuyMaterialAdmin, self).save_model(request, obj, form, change)
 # class HandleMaterialAdmin(admin.ModelAdmin):
 #     class Meta:
 #         model = ApplyMaterial
