d0a375932469b20ee99a5f39d1bafde601ba8f6e
Rita <rita@qq.com>
2016-11-25 20:54:19 +0800

    将app名字汉化。

---
+++ flow/apps.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+from django.apps import AppConfig
+
+class FlowConfig(AppConfig):
+    name = 'flow'
+    verbose_name = u'耗材流程'
---
+++ work/apps.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+from django.apps import AppConfig
+
+class WorkConfig(AppConfig):
+    name = 'work'
+    verbose_name = u'基础信息'
