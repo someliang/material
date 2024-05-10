4d730f1d914e9f454d677ee6b590652aa20589a3
Liang <viso.liang@qq.com>
2024-05-10 18:46:16 +0800

    1.将材料的记录和申请购买流程捆绑，设置为一对一。 2.生成相应的数据迁移文件，并生成数据迁移。 3.完成之前提到的数据结构的代码错误。

--- flow/models.py
+++ flow/models.py
@@ -26,7 +26,7 @@ class ApplyBuyMaterialProcess(models.Model):
         verbose_name=_('apply buy material process')
         verbose_name_plural = _('apply buy material process')
 
-    material_record = models.ForeignKey(MaterialRecord,verbose_name=_('material name'))
+    material_record = models.OneToOneField(MaterialRecord, on_delete=models.CASCADE,verbose_name=_('material name'))
     class_room = models.ForeignKey(ClassRoom, verbose_name = _('apply material class room number'))
     is_agree = models.BooleanField(_('apply status'), default=False)
     is_storage = models.BooleanField(_('storage status'), default=False)
