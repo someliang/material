ec4b58c8cc9f160546204930658dd928b1407b5b
Liang <viso.liang@qq.com>
2024-05-24 22:40:26 +0800

    1.在flow app中添加搜索方法，方便生成ajax即时搜索显示的接口。 2.添加相应的url设置路径。

--- flow/views.py
+++ flow/views.py
@@ -1,3 +1,10 @@
+# -*- coding: utf-8 -*-
+from django.core import serializers
 from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
+from work.models import MaterialRecord
 
-# Create your views here.
+def material_search(request):
+    data = MaterialRecord.objects.filter(name__icontains=request.GET['material_name'])
+    res = serializers.serialize('json', data)
+    return JsonResponse(res, safe=False)
--- material/urls.py
+++ material/urls.py
@@ -13,6 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
+import flow.views
 import settings
 from django.conf.urls import url
 from django.contrib import admin
@@ -21,4 +22,5 @@ from django.views.static import serve
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
+    url(r'^flow/',flow.views.material_search, name='material_search'),
 ]
